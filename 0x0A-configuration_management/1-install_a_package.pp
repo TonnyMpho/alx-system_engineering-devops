501664ac91dc70121960ef475795f871ac7e8d0c
TonnyMpho <tonnympho1@gmail.com>
2023-09-22 11:22:05 -0700

    Configuration management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Manifest to install flask from pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
+}
