31067f01dba5aaaf0d9dccdb084baeafadb70828
TonnyMpho <tonnympho1@gmail.com>
2023-09-24 15:58:23 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # SSH configuration file so that can connect to a server without typing a password.
-file { '~/.ssh/config':
+file { 'config':
   ensure  => 'file',
+  path    => '/etc/ssh/ssh_config''
   content => "\
 Host *
   IdentifyFile ~/.ssh/school
