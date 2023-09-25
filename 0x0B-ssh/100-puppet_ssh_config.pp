7926fa40868032824a51ebf3607bf2b2bda5fa69
TonnyMpho <tonnympho1@gmail.com>
2023-09-24 17:02:49 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # SSH configuration file so that can connect to a server without typing a password.
 file_line { 'disable Password Auth':
   ensure => present,
-  line   => 'passwordAuthentication no',
+  line   => '  passwordAuthentication no',
   path   => '/etc/ssh/ssh_config',
 }
 
 file_line { 'Identity file':
   ensure => present,
-  line   => 'IdentifyFile ~/.ssh/school',
+  line   => '  IdentifyFile ~/.ssh/school',
   path   => '/etc/ssh/ssh_config'
 }
