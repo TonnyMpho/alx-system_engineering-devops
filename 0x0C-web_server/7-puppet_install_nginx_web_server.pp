01bf35d2dbd80a43cadd93a0163bd4c64155902a
TonnyMpho <tonnympho1@gmail.com>
2023-09-27 20:40:06 -0700

    0x0C. Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,43 @@
+# install and configure an Nginx server using Puppet
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html:
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => 'server {
+    listen 80;
+    server_name _;
+
+    location / {
+      root   /var/www/html;
+      index  index.html;
+      add_header Content-Type text/html;
+      echo "Hello World!";
+    }
+
+    location /redirect_me {
+      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+  }',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
