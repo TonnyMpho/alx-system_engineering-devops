9001623bb631e69212d2c6728579ef12a8c326a1
TonnyMpho <tonnympho1@gmail.com>
2023-10-02 16:57:48 -0700

    0x0F. Load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,7 +15,7 @@ file { '/etc/nginx/sites-available/default':
     listen 80 default_server;
     server_name _;
 
-    add_header X-Served-By $(hostname);
+    add_header X-Served-By $hostname;
 
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
