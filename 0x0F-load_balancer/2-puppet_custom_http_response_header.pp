b658546c74635815f2dc833ad0ecf13414596105
TonnyMpho <tonnympho1@gmail.com>
2023-10-02 16:55:13 -0700

    0x0F. Load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,37 @@
+# install and configure an Nginx server using Puppet
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "server {
+    listen 80 default_server;
+    server_name _;
+
+    add_header X-Served-By $(hostname);
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    location / {
+        root /var/www/html;
+        index index.html;
+    }
+}",
+  require => Package['nginx'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
