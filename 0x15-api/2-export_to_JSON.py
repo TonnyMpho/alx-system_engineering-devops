d9652cf3fa4b2c2f0395eedbf9a6a9b1da52f468
TonnyMpho <tonnympho1@gmail.com>
2023-10-23 18:58:32 -0700

    REST API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Python script that use REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        exit(1)
+
+    user_id = argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+    res = requests.get(url + "users/{}".format(user_id))
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    if res.status_code != 200:
+        print("User not found")
+        exit(1)
+
+    username = res.json().get("username")
+
+    data = {user_id: []}
+    for todo in todos:
+        title = todo.get("title")
+        completed = todo.get("completed")
+        data[user_id].append({
+            "task": title,
+            "copleted": completed,
+            "username":username
+            })
+
+    with open("{}.json".format(user_id), 'w') as json_f:
+        json.dump(data, json_f)
