bf7c36b655452f96758739aad2de5e3f790f7878
TonnyMpho <tonnympho1@gmail.com>
2023-11-07 15:24:59 -0800

    API advanced

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Module that contains a function which queries the reddit api """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit
+    """
+    headers = {"User-Agent": "Python/3"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    if after:
+        url += "?after={}".format(after)
+
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 200:
+        data = res.json()
+        posts = data['data']['children']
+
+        for post in posts:
+            hot_list.append(post['data']['title'])
+
+        after = data['data']['after']
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list    
+    else:
+        return None
