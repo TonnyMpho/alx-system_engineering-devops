e4349193ada6494387fdc0d19abd662742cb5e27
TonnyMpho <tonnympho1@gmail.com>
2023-11-08 16:33:17 -0800

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,10 @@
 # Puppet
-file_line { 'Replace':
-  path  => 'var/www/html/wq-settings.php',
-  line  => 'class-wq-locale.php',
-  match => 'class-wq-locale.phpp',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/bin',
 }
 
-exec { 'restart aparche':
+exec { 'restart_aparche':
   command     => '/etc/init.d/apache2 restart',
   refreshonly => true,
   path        => '/usr/bin:/bin',
