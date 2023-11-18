f0d08fbce54d886f519d82c29cd66961f82bffa7
TonnyMpho <tonnympho1@gmail.com>
2023-11-18 15:28:47 -0800

    Web stack debugging #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # change os configuration for holberton user
 
 exec { 'change-os-configuration-for-holberton-user':
- command => 'echo "holberton soft nofile 4096\nholberton har nofile 8192" >> /etc/security/limits.conf',
- path    => ['/usr/bin', '/bin'],
+  command => 'echo "holberton soft nofile 4096\nholberton har nofile 8192" >> /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
 }
