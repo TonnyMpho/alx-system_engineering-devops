31d1e06182c7f4cbf6fa84a1493fb3dec031e4ef
TonnyMpho <tonnympho1@gmail.com>
2023-11-18 14:35:15 -0800

    Web stack debugging #4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # increasing number of requests
 
-exec {'replacing':
+exec { 'replacing':
   command => 'sed -i "s/15/1000/" /etc/default/nginx',
   path    => ['/usr/bin', '/bin'],
 }
 
-exec {'restart_nginx':
+exec { 'restart_nginx':
   command     => '/usr/sbin/service nginx restart',
+  path        => '/usr/sbin',
   refreshonly => true,
 }
